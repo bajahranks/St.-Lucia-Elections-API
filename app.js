17bf65db5c058d5924b630a4e301065de8113f2a
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2021-05-27 02:37:40 -0400

    Remove CORS

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
